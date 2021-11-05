32e0dd4bdbadbddfd675c160ebd16a9f85029e3a
dguba <daniel.guba@logmein.com>
2021-11-05 08:25:38 +0100

    .NET6

--- Client/Program.cs
+++ Client/Program.cs
@@ -19,9 +19,8 @@ namespace rEoP.Client
             CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
             CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddFileReaderService(o => o.UseWasmSharedBuffer = true);
 
